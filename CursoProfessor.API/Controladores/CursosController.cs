70df77daefc1f8791d8c3ad73b33ad8cd4904b2a
Nélio Augusto <106417497+GuguDeaf@users.noreply.github.com>
2022-06-15 21:39:23 -0300

    Projeto de Desafio

---
+++ CursoProfessor.API/Controladores/CursosController.cs
@@ -0,0 +1,53 @@
+﻿using CursoProfessor.Models;
+using CursoProfessor.Services;
+using CursoProfessor.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CursoProfessor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CursosController : ControllerBase
+    {
+        private GestaoServices _cursos =
+           new GestaoServices();
+
+        [HttpPost]
+        public ActionResult CadastrarCursos(
+            [FromBody] CursosViewModel cursosRecebido)
+        {
+            if (cursosRecebido == null)
+            {
+                return BadRequest("Não foi recebido nenhum informação de cursos.");
+            }
+
+            Cursos objetoCriado = _cursos
+                .CadastrarCursos(cursosRecebido);
+
+            return Created("Cursos", objetoCriado);
+        }
+        [HttpGet]
+        public List<Cursos> ListarCursos()
+        {
+            List<Cursos> listaCursos =
+                _cursos.ListarCursos();
+
+            return listaCursos;
+        }
+
+        [HttpPut("{curso_id}")]
+        public void PutCursos(int cursos_id)
+        {
+            Cursos cursos = new Cursos();
+        }
+
+        [HttpDelete("{curso_id}")]
+        public void DeletarCurso(int curso_id)
+        {
+            Cursos cursos = new Cursos();
+        }
+    }
+}
---
+++ CursoProfessor.API/Controladores/ProfessoresController.cs
@@ -0,0 +1,53 @@
+﻿using CursoProfessor.Models;
+using CursoProfessor.Services;
+using CursoProfessor.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CursoProfessor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProfessoresController : ControllerBase
+    {
+        private GestaoServices _professores =
+            new GestaoServices();
+
+        [HttpPost]
+        public ActionResult CadastrarSerie(
+            [FromBody] ProfessoresViewModel professoresRecebido)
+        {
+            if (professoresRecebido == null)
+            {
+                return BadRequest("Não foi recebido nenhum informação de professores.");
+            }
+
+            Professores objetoCriado = _professores
+                .CadastrarProfessores(professoresRecebido);
+
+            return Created("Professores", objetoCriado);
+        }
+        [HttpGet]
+        public List<Professores> ListarProfessores()
+        {
+            List<Professores> listaProfessores =
+                _professores.ListarProfessores();
+
+            return listaProfessores;
+        }
+        [HttpPut("{professores_id}")]
+        public void PutProfessores()
+        {
+            Professores professores = new Professores();
+        }
+
+        [HttpDelete("{professores_id}")]
+        public void DeletarProfessores(int professores_id)
+        {
+            Professores professores = new Professores();
+        }
+
+    }
+}
